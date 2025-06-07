20f8875ef0a47c4ca7e766537375503be5296e4a
Luccas Henrique <86303703+LuccasHenrique13@users.noreply.github.com>
2025-06-07 00:32:49 -0300

    Update

--- src/components/about2.jsx
+++ src/components/about2.jsx
@@ -16,28 +16,33 @@ export function About2() {
 
                 <div className="center sr-fade-up">
                     <div className="grid">
-                        <div className="card sr-sequence-item">
-                            <img src={whatsapp} alt="Ícone WhatsApp" />
-                            <h3>Atendimento rápido</h3>
-                            <p>✔ Direto pelo WhatsApp</p>
+                        <div className="sr-sequence-item">
+                            <div className="card" id="pgbottom">
+                                <img src={whatsapp} alt="Ícone WhatsApp" />
+                                <h3>Atendimento rápido</h3>
+                                <p>✔ Direto pelo WhatsApp</p>
+                            </div>
                         </div>
-
-                        <div className="card sr-sequence-item">
-                            <img src={star} alt="Ícone Qualidade" />
-                            <h3>Qualidade</h3>
-                            <p>✔ Uma qualidade incrível, comparável a peças novas</p>
+                        <div className="sr-sequence-item">
+                            <div className="card">
+                                <img src={star} alt="Ícone Qualidade" />
+                                <h3>Qualidade</h3>
+                                <p>✔ Uma qualidade incrível, comparável a peças novas</p>
+                            </div>
                         </div>
-
-                        <div className="card sr-sequence-item">
-                            <img src={lock} alt="Ícone Garantia" />
-                            <h3>Garantia</h3>
-                            <p>✔ 1 Ano para amortecedores<br />6 meses para qualquer outro produto</p>
+                        <div className="sr-sequence-item">
+                            <div className="card ">
+                                <img src={lock} alt="Ícone Garantia" />
+                                <h3>Garantia</h3>
+                                <p>✔ 1 Ano para amortecedores<br />6 meses para qualquer outro produto</p>
+                            </div>
                         </div>
-
-                        <div className="card sr-sequence-item">
-                            <img src={handshake} alt="Ícone Profissionais" />
-                            <h3>Profissionais experientes</h3>
-                            <p>✔ Com uma ampla experiência no setor automotivo</p>
+                        <div className="sr-sequence-item">
+                            <div className="card">
+                                <img src={handshake} alt="Ícone Profissionais" />
+                                <h3>Profissionais experientes</h3>
+                                <p>✔ Com uma ampla experiência no setor automotivo</p>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -4,6 +4,10 @@ import bandeja from '../assets/bandeja.png';
 import pivo from '../assets/pivo.png';
 import coximdemotor from '../assets/coximdemotor.png';
 import coximdecambio from '../assets/coximdecambio.png';
+import coximdemotorcaminhao from '../assets/coximdemotorcaminhao.jpg';
+import coximdecambiocaminhao from '../assets/coximdecambiocaminhao.jpg';
+import quintaRoda from '../assets/quintaRoda.jpg';
+import coximdebarco from '../assets/coximdebarco.jpg';
 import whatsappLinks from '../data/links.json';
 
 export function Products() {
@@ -13,49 +17,77 @@ export function Products() {
     };
 
     return (
-        <>
-            <div className="bg-products" id="Products">
-                <div className="productsContent sr-fade-up ">
-                    <h1 className="productsTitle">Produtos</h1>
+        <div className="bg-products" id="Products">
+            <div className="productsContent sr-fade-up">
+                <h1 className="productsTitle">Produtos</h1>
+            </div>
+            <div className="productsGrid sr-fade-up">
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={amortecedor} alt="Amortecedores" />
+                        <h2 className="imageTitle">Amortecedores</h2>
+                        <a className="buttonAboutProducts" href={getLink("Amortecedores")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
+                    </div>
                 </div>
-                <div className="productsGrid sr-fade-up">
-                    <div className="productsCard sr-sequence-item">
-                        <div className="imageWrapper">
-                            <img src={amortecedor} alt="Amortecedores" />
-                            <h2 className="imageTitle">Amortecedores</h2>
-                            <a className="buttonAboutProducts" href={getLink("Amortecedores")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
-                        </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={bandeja} alt="Bandejas" />
+                        <h2 className="imageTitle">Bandejas</h2>
+                        <a className="buttonAboutProducts" href={getLink("Bandejas")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
                     </div>
-                    <div className="productsCard sr-sequence-item">
-                        <div className="imageWrapper">
-                            <img src={bandeja} alt="Bandejas" />
-                            <h2 className="imageTitle">Bandejas</h2>
-                            <a className="buttonAboutProducts" href={getLink("Bandejas")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
-                        </div>
+                </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={pivo} alt="Pivôs" />
+                        <h2 className="imageTitle">Pivôs</h2>
+                        <a className="buttonAboutProducts" href={getLink("Pivôs")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
                     </div>
-                    <div className="productsCard sr-sequence-item">
-                        <div className="imageWrapper">
-                            <img src={pivo} alt="Pivôs" />
-                            <h2 className="imageTitle">Pivôs</h2>
-                            <a className="buttonAboutProducts" href={getLink("Pivôs")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
-                        </div>
+                </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={coximdemotor} alt="Coxim de Motor" />
+                        <h2 className="imageTitle">Coxim de Motor</h2>
+                        <a className="buttonAboutProducts" href={getLink("Coxim de Motor")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
                     </div>
-                    <div className="productsCard sr-sequence-item">
-                        <div className="imageWrapper">
-                            <img src={coximdemotor} alt="Coxim de Motor" />
-                            <h2 className="imageTitle">Coxim de Motor</h2>
-                            <a className="buttonAboutProducts" href={getLink("Coxim de Motor")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
-                        </div>
+                </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={coximdecambio} alt="Coxim de Câmbio" />
+                        <h2 className="imageTitle">Coxim de Câmbio</h2>
+                        <a className="buttonAboutProducts" href={getLink("Coxim de Câmbio")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
                     </div>
-                    <div className="productsCard sr-sequence-item">
-                        <div className="imageWrapper">
-                            <img src={coximdecambio} alt="Coxim de Câmbio" />
-                            <h2 className="imageTitle">Coxim de Câmbio</h2>
-                            <a className="buttonAboutProducts" href={getLink("Coxim de Câmbio")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
-                        </div>
+                </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={coximdemotorcaminhao} alt="Coxim de Motor de Caminhão" />
+                        <h2 className="imageTitle"id='top'>Coxim de Motor <br /> de Caminhão</h2>
+                        <a className="buttonAboutProducts" href={getLink("Coxim de Motor de Caminhão")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
+                    </div>
+                </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={coximdecambiocaminhao} alt="Coxim de Câmbio de Caminhão" />
+                        <h2 className="imageTitle"id='top'>Coxim de Câmbio <br /> de Caminhão</h2>
+                        <a className="buttonAboutProducts" href={getLink("Coxim de Câmbio de Caminhão")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
+                    </div>
+                </div>
+                <div className="productsCard sr-sequence-item">
+                    <div className="imageWrapper">
+                        <img src={quintaRoda} alt="Coxim quinta roda de caminhão" />
+                        <h2 className="imageTitle"id='top'>Coxim Quinta<br /> Roda de Caminhão</h2>
+                        <a className="buttonAboutProducts" href={getLink("Coxim Quinta Roda Caminhão")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
+                    </div>
+                </div>
+
+                {/* Última imagem centralizada */}
+                <div className="productsCard sr-sequence-item last-product">
+                    <div className="imageWrapper">
+                        <img src={coximdebarco} alt="Coxim de Barco" />
+                        <h2 className="imageTitle">Coxim de Barco</h2>
+                        <a className="buttonAboutProducts" href={getLink("Coxim de Barco")} target="_blank" rel="noopener noreferrer">Saiba mais</a>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
